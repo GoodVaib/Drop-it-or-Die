825db5983c2310b6baadb99005e0ad55258c22b6
Crufix <155760866+GoodVaib@users.noreply.github.com>
2025-09-16 14:02:14 +0300

    Changed font and color labels

--- src/main.cpp
+++ src/main.cpp
@@ -17,25 +17,36 @@ using namespace sf;
 
 /////////////////////
 
+
+// variables
+static short score_pl1_short, score_pl2_short, number_cube1_pl1, number_cube2_pl1, number_cube1_pl2, number_cube2_pl2;
+
+
+// main func
 int main()
 {
     RenderWindow window{VideoMode{ {800, 600} }, "Drop it or Die"};
     Gui gui{window};
     
-     
     auto score_pl1_text = tgui::Label::create("Score: 0");
+    score_pl1_text->getRenderer()->setTextColor(tgui::Color::White);
+    score_pl1_text->getRenderer()->setFont("./fonts/Hero-Bold.ttf");
     score_pl1_text->setPosition("2%", "2%");
     score_pl1_text->setTextSize(18);
     score_pl1_text->setOrigin(0, 0);
     gui.add(score_pl1_text);
 
     auto score_pl2_text = tgui::Label::create("Score: 0");
+    score_pl2_text->getRenderer()->setTextColor(tgui::Color::White);
+    score_pl2_text->getRenderer()->setFont("./fonts/Hero-Bold.ttf");
     score_pl2_text->setPosition("98%", "2%");
     score_pl2_text->setTextSize(18);
     score_pl2_text->setOrigin(1, 0);
     gui.add(score_pl2_text);
 
     auto btn_tap = Button::create("Click me!");
+    //btn_tap->getRenderer()->setTextColor(tgui::Color::White);
+    btn_tap->getRenderer()->setFont("./fonts/Hero-Bold.ttf");
     btn_tap->setPosition("50%", "50%");
     btn_tap->setOrigin(0.5, 0.5);
     btn_tap->onPress([&]{
@@ -52,11 +63,11 @@ int main()
         while (const std::optional event = window.pollEvent()) {
             gui.handleEvent(*event);
             
-            //window.close();
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
 
+        // render
         window.clear({62, 35, 0});
         gui.draw();
         window.display();
